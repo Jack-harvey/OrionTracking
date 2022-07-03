ad7166ee2440ad7080bb863ef98fc19f696a94ae
Jack Harvey <JackHarvey.00@gmail.com>
2022-07-03 21:31:53 +1000

    Clear up folder structure

---
+++ OrionTracking/wwwroot/js/DevExtreme/asset-index.js
@@ -0,0 +1,76 @@
+﻿$(function () {
+    let serviceUrl = "https://localhost:7029/assets";
+    DevExpress.localization.locale("en-AU");
+    $("#dataGridContainer").dxDataGrid({
+        // ...
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            loadUrl: serviceUrl + "/GetAction",
+            //insertUrl: serviceUrl + "/InsertAction",
+            //updateUrl: serviceUrl + "/UpdateAction",
+            //deleteUrl: serviceUrl + "/DeleteAction",
+        }),
+        showColumnLines: false,
+        showRowLines: true,
+        rowAlternationEnabled: true,
+        showBorders: true,
+        columns: [{
+            dataField: "companyTrackingId",
+            caption: "Asset-Id"
+        }, {
+            dataField: "name",
+            caption: "Name"
+        }, {
+            dataField: "typeName",
+            caption: "Type"
+        }, {
+            dataField: "userName",
+            caption: "Employee"
+        },
+        {
+            dataField: "location",
+            caption: "Location"
+        },
+        {
+            dataField: "purchaseDate",
+            caption: "Purchase-Date",
+            dataType: "date",
+            format: 'dd-MMM-yyyy'
+        },
+
+        {
+            type: 'buttons',
+            width: 90,
+            buttons: [{
+                hint: 'Details',
+                icon: 'card',
+                onClick(e) {
+                    //const editItemLink = (`edit/${e.id}`);
+                    const newLink = (`${window.location.origin}/Assets/details/${e.row.key}`);
+                    window.location = newLink;
+
+                },
+            }, {
+                hint: 'Edit',
+                icon: 'edit',
+                onClick(e) {
+                    //const editItemLink = (`edit/${e.id}`);
+                    const newLink = (`${window.location.origin}/Assets/edit/${e.row.key}`);
+                    window.location = newLink;
+
+                },
+            }, {
+                    hint: 'delete',
+                    icon: 'trash',
+                    onClick(e) {
+                        //const editItemLink = (`edit/${e.id}`);
+                        const newLink = (`${window.location.origin}/Assets/delete/${e.row.key}`);
+                        window.location = newLink;
+
+                    },
+                }],
+        },
+        ],
+        allowColumnReordering: true,
+    })
+});
