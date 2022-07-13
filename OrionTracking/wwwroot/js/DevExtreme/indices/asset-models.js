9be650d17a3bfd2ee7e9272f59c746c95bc901d3
Jack Harvey <JackHarvey.00@gmail.com>
2022-07-13 21:14:11 +1000

    Restructure js folder

---
+++ OrionTracking/wwwroot/js/DevExtreme/indices/asset-models.js
@@ -0,0 +1,80 @@
+﻿$(function () {
+    let serviceUrl = "https://localhost:7029/employees";
+    DevExpress.localization.locale("en-AU");
+    $("#dataGridContainer").dxDataGrid({
+        // ...
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            loadUrl: serviceUrl + "/GetAction",
+            //insertUrl: serviceUrl + "/InsertAction",
+            //updateUrl: serviceUrl + "/UpdateAction",
+            //deleteUrl: serviceUrl + "/DeleteAction",
+        }),
+        showColumnLines: false,
+        showRowLines: true,
+        rowAlternationEnabled: true,
+        showBorders: true,
+        columns: [{
+            dataField: "firstName",
+            caption: "Name"
+        }, {
+            dataField: "lastName",
+            caption: "Surname"
+        }, {
+            dataField: "userName",
+            caption: "Username"
+        }, {
+            dataField: "name",
+            caption: "Job title"
+        },
+        {
+            dataField: "email",
+            caption: "E-mail"
+        },
+        {
+            dataField: "city",
+            caption: "Office"
+        },
+        {
+            dataField: "startDate",
+            caption: "Start-Date",
+            dataType: "date",
+            format: 'dd-MMM-yyyy'
+        },
+
+        {
+            type: 'buttons',
+            width: 90,
+            buttons: [{
+                hint: 'Details',
+                icon: 'card',
+                onClick(e) {
+                    //const editItemLink = (`edit/${e.id}`);
+                    const newLink = (`${window.location.origin}/Employees/details/${e.row.key}`);
+                    window.location = newLink;
+
+                },
+            }, {
+                hint: 'Edit',
+                icon: 'edit',
+                onClick(e) {
+                    //const editItemLink = (`edit/${e.id}`);
+                    const newLink = (`${window.location.origin}/Employees/edit/${e.row.key}`);
+                    window.location = newLink;
+
+                },
+            }, {
+                    hint: 'delete',
+                    icon: 'trash',
+                    onClick(e) {
+                        //const editItemLink = (`edit/${e.id}`);
+                        const newLink = (`${window.location.origin}/Employees/delete/${e.row.key}`);
+                        window.location = newLink;
+
+                    },
+                }],
+        },
+        ],
+        allowColumnReordering: true,
+    })
+});
